2297f8923a48fd183487448ffe21f9b4c41ae9da
traxaber <traxaber@gmail.com>
2023-12-06 16:14:52 +1100

    day5 (maybe)

---
+++ day5/pt2.py
@@ -0,0 +1,29 @@
+f = open("input.txt", "r").read().splitlines()
+
+seeds = f[0].replace("seeds:","").split()
+minimumOutcome = int(seeds[0]) 
+for j in range(0, len(seeds), 2):
+    print(j)
+    for currentSeed in range(int(seeds[j]), int(seeds[j]) + int(seeds[j+1])):
+        mapping = currentSeed
+        found = False
+        i = 3
+        while i < len(f) - 1:
+            if found:
+                i+=1
+            l = f[i]
+            if len(l) > 0 and l[0].isnumeric():
+                source = int(l.split()[1])
+                dest = int(l.split()[0])
+                seedRange = int(l.split()[2])
+                diff = dest - source
+                if source <= currentSeed and currentSeed < source + seedRange:
+                    mapping = currentSeed + diff
+                    found = True
+            else:
+                currentSeed = mapping
+                found = False
+            i+=1
+        if mapping < minimumOutcome:
+            minimumOutcome = mapping
+print(minimumOutcome)
