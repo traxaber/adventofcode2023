ded548983d6e25dec295fc5262f4c5625086a81b
traxaber <traxaber@gmail.com>
2023-12-08 20:46:40 +1100

    day5 (had to look at other solutions but yeah lol)
    My initial solution was way too slow so I had to look up
    other people's solutions to find out going backwards is
    faster. Then I had to use someone else's code to find out
    my solution, which revealed that it was really low and my
    code was passing by it. Then the fix was clear and I got the
    solution with my code (very quickly because the solution was
    literally the minimum result lol)

--- day5/pt2.py
+++ day5/pt2.py
@@ -1,29 +1,34 @@
 f = open("input.txt", "r").read().splitlines()
 
-seeds = f[0].replace("seeds:","").split()
-minimumOutcome = int(seeds[0]) 
-for j in range(0, len(seeds), 2):
-    print(j)
-    for currentSeed in range(int(seeds[j]), int(seeds[j]) + int(seeds[j+1])):
-        mapping = currentSeed
-        found = False
-        i = 3
-        while i < len(f) - 1:
-            if found:
-                i+=1
-            l = f[i]
-            if len(l) > 0 and l[0].isnumeric():
-                source = int(l.split()[1])
-                dest = int(l.split()[0])
-                seedRange = int(l.split()[2])
-                diff = dest - source
-                if source <= currentSeed and currentSeed < source + seedRange:
-                    mapping = currentSeed + diff
-                    found = True
-            else:
-                currentSeed = mapping
-                found = False
-            i+=1
-        if mapping < minimumOutcome:
-            minimumOutcome = mapping
-print(minimumOutcome)
+seeds = f[0].replace("seeds: ", "").split()
+
+def inSeeds(mapping):
+    for i in range(0, len(seeds), 2):
+        if mapping >= int(seeds[i]) and mapping < int(seeds[i]) + int(seeds[i+1]):
+            return True
+    return False
+
+start = int(f[-1].split()[0])
+for l in reversed(f):
+    if l[0].isalpha():
+        break
+    if int(l.split()[0]) < start:
+        start = int(l.split()[0])
+
+done = False
+while True:
+    mapping = start
+    for l in reversed(f[3:]):
+        l = l.split()
+        if len(l) == 0 or l[0][0].isalpha():
+            done = False
+            continue
+        elif done == True:
+            continue
+        if  mapping >= int(l[0]) and mapping < int(l[0]) + int(l[2]):
+            mapping = mapping - int(l[0]) + int(l[1])
+            done = True 
+    if inSeeds(mapping):
+        print(start)
+        break
+    start += 1
